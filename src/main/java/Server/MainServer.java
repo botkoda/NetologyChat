641dea6a8d59157e6c859a6cb13b194d28dbc44a
ДЕНИС <52046838+botkoda@users.noreply.github.com>
2021-11-22 08:39:10 +0400

    Update MainServer.java

--- src/main/java/Server/MainServer.java
+++ src/main/java/Server/MainServer.java
@@ -2,6 +2,7 @@ package Server;
 
 public class MainServer {
     public static void main(String[] args) {
-        new SocketServer(Config.HOST,Config.PORT).start();
+        
+        new SocketServer(Config.HOST, Config.PORT).start();
     }
 }
